b497170e3d3138d30369802cd6716e0fe6292374
Yossi4444 <704402124@qq.com>
2023-01-18 23:06:49 +0800

    springboot框架搭建

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -52,17 +52,9 @@
               <span slot="title">导航三</span>
             </template>
             <el-menu-item-group>
-              <template slot="title">分组一</template>
               <el-menu-item index="3-1">选项1</el-menu-item>
               <el-menu-item index="3-2">选项2</el-menu-item>
             </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="3-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="3-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-            </el-submenu>
           </el-submenu>
         </el-menu>
       </el-aside>
@@ -82,14 +74,48 @@
         </el-header>
 
         <el-main>
-          <el-table :data="tableData">
-            <el-table-column prop="date" label="日期" width="140">
+          <div style="margin-bottom: 30px">
+            <el-breadcrumb separator="/">
+              <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+              <el-breadcrumb-item><a href="/">油井管理</a></el-breadcrumb-item>
+            </el-breadcrumb>
+          </div>
+
+          <div style="margin: 10px 0">
+            <el-input style="width:200px"  placeholder="请输入编号" suffix-icon="el-icon-search"></el-input>
+            <el-input style="width:200px"  placeholder="请输入名称" suffix-icon="el-icon-message" class="ml-5"></el-input>
+            <el-input style="width:200px"  placeholder="请输入地址" suffix-icon="el-icon-position" class="ml-5"></el-input>
+            <el-button class="ml-5" type="primary">搜索</el-button>
+          </div>
+          <div style="margin: 10px 0">
+            <el-button type="primary">新增<i class="el-icon-circle-plus-outline"></i></el-button>
+            <el-button type="danger">批量删除<i class="el-icon-remove-outline"></i></el-button>
+            <el-button type="primary">导入<i class="el-icon-top"></i></el-button>
+            <el-button type="primary">导出<i class="el-icon-bottom"></i></el-button>
+          </div>
+
+          <el-table :data="tableData" border stripe>
+            <el-table-column prop="number" label="名称" width="140">
             </el-table-column>
-            <el-table-column prop="name" label="姓名" width="120">
+            <el-table-column prop="name" label="编号" width="120">
             </el-table-column>
             <el-table-column prop="address" label="地址">
             </el-table-column>
+            <el-table-column>
+              <template slot-scope="scope">
+                <el-button type="warning">编辑<i class="el-icon-edit"></i></el-button>
+                <el-button type="danger">删除<i class="el-icon-remove-outline"></i></el-button>
+              </template>
+            </el-table-column>
           </el-table>
+          <div style="padding: 10px 0">
+            <el-pagination
+                :page-sizes="[5, 10, 15, 20]"
+                :page-size="10"
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="400">
+            </el-pagination>
+          </div>
         </el-main>
       </el-container>
     </el-container>
@@ -102,7 +128,7 @@ export default {
   name: 'Home',
   data() {
     const item = {
-      date: '2016-05-02',
+      number: '0001',
       name: '油气井',
       address: '新疆塔里木盆地顺北油气田2号带'
     };
