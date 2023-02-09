d7d09522001465e14124c02fca0106c36f654b83
Yossi4444 <704402124@qq.com>
2023-02-09 23:14:18 +0800

    更新

--- src/main/java/com/xyx/springbootdemo/entity/User.java
+++ src/main/java/com/xyx/springbootdemo/entity/User.java
@@ -1,11 +1,13 @@
 package com.xyx.springbootdemo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 @Data
 public class User {
     private Integer id;
     private String username;
+    @JsonIgnore//不展示密码
     private String password;
     private String nickname;
     private String email;
